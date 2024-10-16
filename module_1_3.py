a9550ccb05f793f788ad1e444728a8feb87864cd
Miky N <something.is.somewhere27@gmail.com>
2024-10-16 17:22:42 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Miky'
+r_name = "Don't you care?"
+print(f'Name: {name}, Real Name: {r_name}')
+age = 17
+print(f'Age: {age}')
+age = age + 1
+print(f'New Age: {age}')
+is_human = True
+print(f'Is Human: {is_human}')
