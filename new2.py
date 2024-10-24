b0a0a784d1224a0ac559354951a9ed9a0c9a0de4
Miky N <something.is.somewhere27@gmail.com>
2024-10-24 22:54:13 +0500

    Add files via upload

---
+++ new2.py
@@ -0,0 +1,8 @@
+first = input('Input first number: ')
+second = input('Input second number: ')
+third = input('Input third number: ')
+if first==second and second==third:
+    print(3 'are the same')
+elif first==second or second==third or first==third:
+    print(2 'are the same')
+else: print(0 'are the same')
